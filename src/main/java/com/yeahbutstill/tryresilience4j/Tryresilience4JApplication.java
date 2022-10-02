09bbab8a94af9f5b98c2966ec9a312f4aef7daf3
yeahbutstill <mayadani28@gmail.com>
2022-10-02 23:12:04 +0700

    setup project

---
+++ src/main/java/com/yeahbutstill/tryresilience4j/Tryresilience4JApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.tryresilience4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tryresilience4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tryresilience4JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/tryresilience4J/Tryresilience4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.tryresilience4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tryresilience4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
