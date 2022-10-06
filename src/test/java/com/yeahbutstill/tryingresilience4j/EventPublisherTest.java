3e7b4693087d6424faa2805e4bf70f0ab05003b3
yeahbutstill <mayadani28@gmail.com>
2022-10-06 22:00:59 +0700

    play with event publisher

---
+++ src/test/java/com/yeahbutstill/tryingresilience4j/EventPublisherTest.java
@@ -0,0 +1,47 @@
+package com.yeahbutstill.tryingresilience4j;
+
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+@Slf4j
+class EventPublisherTest {
+
+    @Test
+    void retry() {
+        Retry retry = Retry.ofDefaults("yeahbutstill");
+        retry.getEventPublisher().onRetry(event -> {
+            log.info("try to retry");
+        });
+
+        try {
+            Supplier<String> supplier = Retry.decorateSupplier(retry, () -> hello());
+            supplier.get();
+        } catch (Exception e) {
+            System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithoutRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfFailedCallsWithRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt());
+        }
+    }
+
+    private String hello() {
+        throw new IllegalArgumentException("Ups");
+    }
+
+    @Test
+    void registry() {
+        RetryRegistry registry = RetryRegistry.ofDefaults();
+        registry.getEventPublisher().onEntryAdded(event ->  {
+            log.info("Add new entry {}", event.getAddedEntry().getName());
+        });
+
+        registry.retry("yeahbutstill");
+        registry.retry("yeahbutstill");
+        registry.retry("yeahbutstill2");
+    }
+
+}
